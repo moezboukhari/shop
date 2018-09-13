7d8208a5c842c606a3e96ccef5770f5a8151b6c4
moezboukhari <moez.boukhari@gmail.com>
2018-09-13 17:33:14 +0100

    first init whith spring

---
+++ shop-BackEnd/src/main/java/org/sid/shopBackEnd/ShopBackEndApplication.java
@@ -0,0 +1,12 @@
+package org.sid.shopBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBackEndApplication.class, args);
+	}
+}
---
+++ shop-FrontEnd/src/main/java/org/sid/shopFrontEnd/ShopFrontEndApplication.java
@@ -0,0 +1,12 @@
+package org.sid.shopFrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopFrontEndApplication.class, args);
+	}
+}
